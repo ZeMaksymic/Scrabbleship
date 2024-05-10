de4ef2d61591d05c1f917941a2cff08120e0b4ae
Ryan Maksymic <ryan.maksymic@zemind.ca>
2024-05-10 10:58:32 -0400

    Created GridButton

---
+++ Scrabbleship/Assets/Scripts/GridButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridButton : MonoBehaviour
+{
+    [SerializeField] public int row;
+    [SerializeField] public int column;
+
+    public char letter = ' ';
+}
