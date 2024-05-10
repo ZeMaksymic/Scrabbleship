6edc7d155d827fd22048f521489a2fc581f55a2c
Ryan Maksymic <ryan.maksymic@zemind.ca>
2024-05-10 15:04:45 -0400

    Added text label and UI methods to GridButton

--- Scrabbleship/Assets/Scripts/GridButton.cs
+++ Scrabbleship/Assets/Scripts/GridButton.cs
@@ -1,11 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 
 public class GridButton : MonoBehaviour
 {
-    [SerializeField] public int row;
-    [SerializeField] public int column;
+    [SerializeField] TMP_Text letterText;
 
-    public char letter = ' ';
+    public int row;
+    public int column;
+    public char letter;
+
+    public void SetData(Tuple<int,int> _coords, char _letter = ' ')
+    {
+        row = _coords.Item1;
+        column = _coords.Item2;
+        letter = _letter;
+    }
+
+    public void SetYellow(char _letter)
+    {
+        GetComponent<UnityEngine.UI.Image>().color = Color.yellow;
+        letterText.text = $"{_letter}";
+    }
+
+    public void SetGreen(char _letter)
+    {
+        GetComponent<UnityEngine.UI.Image>().color = Color.green;
+        letterText.text = $"{_letter}";
+    }
 }
